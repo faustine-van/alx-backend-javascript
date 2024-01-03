db497ba5bbe074799c6fed2cdc055df02fd7425f
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-03 22:08:23 +0200

    fix Unary operator ++ used and unamed functions

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,25 @@
 export default function createIteratorObject(report) {
-  return [...Object.values(report.allEmployees)[0], ...Object.values(report.allEmployees)[1]];
+  const departments = Object.values(report.allEmployees);
+  let currentDepartmentIndex = 0;
+  let currentEmployeeIndex = 0;
+
+  const iterator = {
+    next() {
+      if (currentEmployeeIndex < departments[currentDepartmentIndex].length) {
+        const employee = departments[currentDepartmentIndex][currentEmployeeIndex];
+        currentEmployeeIndex += 1;
+        return { value: employee, done: false };
+      }
+      currentDepartmentIndex += 1;
+      currentEmployeeIndex = 0;
+      if (currentDepartmentIndex < departments.length) {
+        return this.next();
+      }
+      return { done: true };
+    },
+  };
+  iterator[Symbol.iterator] = function namedIterator() {
+    return this;
+  };
+  return iterator;
 }
