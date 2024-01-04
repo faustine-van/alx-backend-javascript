8f5ea20fa850663829cfe8f9b2f48c40d27b79da
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-04 16:59:11 +0200

    promise.race() fast winner always win

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function asyncUploadUser() {
+  const uploaded = uploadPhoto();
+  const created = createUser();
+  return Promise.all([uploaded, created])
+    .then((values) => {
+      const [val1, val2] = values;
+      return {
+        photo: val1,
+        user: val2,
+      };
+    });
+}
