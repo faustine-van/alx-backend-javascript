b0e55264e8c3a91bfecbca66b58a504ada650a5e
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-05 00:04:27 +0200

    setter and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,10 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     this._name = name;
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._length = length;
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+    this._length = length;
     this._students = students;
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array');
