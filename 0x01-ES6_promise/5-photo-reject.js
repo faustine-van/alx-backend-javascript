39c2dabaae9ff93638bb53ea3f0dc608ca8e5cfa
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-04 11:30:25 +0200

    returm promise with reject error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function uploadPhoto(filename) {
-  const promise = new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
+  const promise = new Promise((resolve, reject) => {
+    reject(Error(`${filename} cannot be processed`));
   });
   return promise;
 }
