62c5aebcf192051521af36fba57ee1f959394e33
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-04 17:16:24 +0200

    promise.race() fast winner always win

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,12 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   const uploaded = uploadPhoto();
   const created = createUser();
-  return Promise.all([uploaded, created])
-    .then((values) => {
-      const [val1, val2] = values;
-      return {
-        photo: val1,
-        user: val2,
-      };
-    });
+  const [val1, val2] = await Promise.all([uploaded, created]);
+  return {
+    photo: val1,
+    user: val2,
+  };
 }
