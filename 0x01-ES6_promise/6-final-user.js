4baa35042ac7ee09e18a389e40e01fe81ab781d9
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-04 13:09:32 +0200

    Promise.allSettled()

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,24 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      console.log(values);
-    }).catch((e) => e);
+  const promises = [];
+
+  const userPromise = signUpUser(firstName, lastName);
+  promises.push(userPromise);
+
+  const photoPromise = uploadPhoto(fileName);
+  promises.push(photoPromise);
+
+  return Promise.allSettled(promises).then((results) => results.map((res) => {
+    if (res.status === 'fulfilled') {
+      return {
+        status: 'fulfilled',
+        value: res.value,
+      };
+    }
+    return {
+      status: 'rejected',
+      value: res.reason.message,
+    };
+  }));
 }
