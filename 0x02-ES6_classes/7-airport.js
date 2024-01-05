f809ab2db8162ba21e69b441aa7a78d582b78c20
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-05 14:23:59 +0200

    Airport class

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,34 +4,7 @@ export default class Airport {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
-
-    if (typeof code !== 'string') {
-      throw new TypeError('code must be a string');
-    }
-
     this._code = code;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    this._name = newName;
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(newNcode) {
-    this._code = newNcode;
-    if (typeof newcode !== 'string') {
-      throw new TypeError('code must be a string');
-    }
   }
 
   toString() {
