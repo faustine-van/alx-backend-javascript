d7f65fc61440062fe19584fe9f3305a21577494a
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-05 23:44:37 +0200

    primitive value

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building  && typeof this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
