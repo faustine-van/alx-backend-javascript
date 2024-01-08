135d53c8702bfef483c08e4a9d385b9bc41954b8
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-08 21:28:31 +0200

    typed array, map, set

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
-export default function createInt8TypedArray(length, position, value){
-    if (position + 1 > length){
-        return ('Position outside range');
-    }
-    const newBuffer =  new  ArrayBuffer(length);
-    const view = new DataView(newBuffer);
-    view.setInt8(position, value);
-    return view;
+export default function createInt8TypedArray(length, position, value) {
+  if (position > length) {
+    return ('Position outside range');
+  }
+  const newBuffer = new ArrayBuffer(length);
+  const view = new DataView(newBuffer);
+  view.setInt8(position, value);
+  return view;
 }
