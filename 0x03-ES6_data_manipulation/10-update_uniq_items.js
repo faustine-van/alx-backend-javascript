6e05d05a1af89692cf768927bd732dfd8a172aa8
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-08 22:44:16 +0200

    set value andd key new map, set

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function groceriesList(map) {
   // myMap.forEach((value, key) => { if (value === 10) {
   // myMap.set(key, newValue);}});
-  if (typeof map !== 'object'){
+  if (typeof map !== 'object') {
     throw new Error('Cannot process');
   }
   for (const [key, val] of map.entries()) {
