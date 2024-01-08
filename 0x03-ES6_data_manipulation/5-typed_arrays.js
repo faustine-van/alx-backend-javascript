e9b50556d5a1c4d0cddc571cd04e74a397b04e3e
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-08 20:58:33 +0200

    typed array, map, set

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value){
+    if (position + 1 > length){
+        return ('Position outside range');
+    }
+    const newBuffer =  new  ArrayBuffer(length);
+    const view = new DataView(newBuffer);
+    view.setInt8(position, value);
+    return view;
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString){
+  if (startString === ''){
+    return '';
+  }
+  let text = '';
+  set.forEach((value) => {
+    const len = startString.length;
+    if (value.startsWith(startString)){
+        const val = value.slice(len) + '-';
+        text += val
+    }
+});
+  return text.slice(0, -1)
+}
