914cc629aae642f22af95141d124f5e167ee605d
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-09 10:53:49 +0200

    string manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0) {
+  if (!startString || startString.length === 0 || !(set instanceof Set)) {
     return '';
   }
   let text = '';
