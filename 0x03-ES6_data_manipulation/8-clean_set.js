bfe8f6994f448e1a5315ab9019a893d0331eaf9b
faustine-van <faustinemuhayemariya44@gmail.com>
2024-01-09 10:57:59 +0200

    string manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,17 @@
 export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0 || !(set instanceof Set)) {
+  if (!startString || startString.length === 0) {
     return '';
   }
-  let text = '';
+  const newArray = [];
+  //let text = '';
   set.forEach((value) => {
     const len = startString.length;
     if (value.startsWith(startString)) {
-      const val = `${value.slice(len)}-`;
-      text += val;
+      const val = `${value.slice(len)}`;
+      newArray.push(val);
+      //text += val;
     }
   });
-  return text.slice(0, -1);
+  //return text.slice(0, -1);
+  return newArray.join('-');
 }
