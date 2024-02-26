1a1e188ea72c75788454a98dfb1cc67ee94eafac
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-26 21:39:44 +0200

    Process stdin

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+// Reading a file synchronously with Node JS
+const fs = require('fs');
+
+function countStudents(file) {
+  fs.readFile(file, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    let j = 0;
+    let u = 0;
+    const students = [];
+    const args = data.split('\n');
+    const items = args.slice(1, -1);
+    console.log(`Number of students: ${items.length}`);
+    for (const item of items) {
+      students.push(item.split(','));
+    }
+    const csList = [];
+    const sweList = [];
+    for (const student of students) {
+      // console.log(student)
+      for (const s of student) {
+        if (s === 'CS') {
+          u += 1;
+          csList.push(student[0]);
+        } else if (s === 'SWE') {
+          j += 1;
+          sweList.push(student[0]);
+        }
+      }
+    }
+    console.log(`Number of students in CS: ${u}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${j}. List: ${sweList.join(', ')}`);
+  });
+}
+module.exports = countStudents;
