2d105a1f6e96e4abdf8df5eb8edd4a064a5d8a52
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-26 22:16:12 +0200

    http create server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,11 @@
-// Create a small HTTP server using Node's HTTP module
 const http = require('http');
 
+// Create HTTP server
 const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello Holberton School!');
-    res.end();
+  // set header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!\n');
 }).listen(1245);
 
+// Export the app for external use
 module.exports = app;
