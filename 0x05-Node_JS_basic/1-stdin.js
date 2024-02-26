25eada24a0bb20cdcff525e5068ec835b04d9be3
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-26 21:58:27 +0200

    Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,13 +2,10 @@
 const process = require('process');
 // Set encoding for stdin
 process.stdin.setEncoding('utf-8');
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const INPUT = process.stdin.read();
-  if (INPUT) {
-    process.stdout.write(`Your name is: ${INPUT}`);
-  }
+process.stdin.on('data', (INPUT) => {
+  process.stdout.write(`Your name is: ${INPUT}`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
