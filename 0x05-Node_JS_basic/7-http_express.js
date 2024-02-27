6ff5bb65ce29ffebd09b8b6e947f04867f8b9179
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-27 13:24:49 +0200

    updated and solve read 3-read_file_async.js as content

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -12,34 +12,38 @@ app.get('/', (req, res) => {
 });
 
 async function countStudents(file) {
-  const response = await fs.readFile(file, 'utf-8');
-  const lines = response.split('\n').filter((line) => line.trim() !== '');
-  const students = lines.slice(1).map((line) => line.split(','));
+  try {
+    const response = await fs.readFile(file, 'utf-8');
+    const lines = response.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
 
-  let sweCount = 0;
-  let csCount = 0;
-  const csList = [];
-  const sweList = [];
+    let sweCount = 0;
+    let csCount = 0;
+    const csList = [];
+    const sweList = [];
 
-  for (const student of students) {
-    for (const field of student) {
-      // if (fields.includes('CS')) {
-      if (field === 'CS') {
-        csCount += 1;
-        csList.push(student[0]);
-      } else if (field === 'SWE') {
-        sweCount += 1;
-        sweList.push(student[0]);
+    for (const student of students) {
+      for (const field of student) {
+        // if (fields.includes('CS')) {
+        if (field === 'CS') {
+          csCount += 1;
+          csList.push(student[0]);
+        } else if (field === 'SWE') {
+          sweCount += 1;
+          sweList.push(student[0]);
+        }
       }
     }
+    const reportParts = [
+      'This is the list of our students',
+      `Number of students: ${students.length}`,
+      `Number of students in CS: ${csCount}. List: ${csList.join(', ')}`,
+      `Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`,
+    ];
+    return reportParts.join('\n');
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-  const reportParts = [
-    'This is the list of our students',
-    `Number of students: ${students.length}`,
-    `Number of students in CS: ${csCount}. List: ${csList.join(', ')}`,
-    `Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`,
-  ];
-  return reportParts.join('\n');
 }
 // Route for the /students path
 app.get('/students', async (req, res) => {
