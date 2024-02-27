c7b9bb2199618ed8decc5d70203e413684ddb199
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-27 13:13:40 +0200

    updated and solve read 3-read_file_async.js as content

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,39 +3,34 @@ const fs = require('fs').promises;
 
 async function countStudents(file) {
   try {
-    const res = await fs.readFile(file, 'utf-8');
+    const response = await fs.readFile(file, 'utf-8');
+    const lines = response.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
+
     let sweCount = 0;
     let csCount = 0;
-    const students = [];
-    const args = res.split('\n');
-    const items = args.slice(1, -1);
-    console.log(`Number of students: ${items.length}`);
-    for (const item of items) {
-      students.push(item.split(','));
-    }
     const csList = [];
     const sweList = [];
+
     for (const student of students) {
-      for (const s of student) {
-        if (s === 'CS') {
+      for (const field of student) {
+        // if (fields.includes('CS')) {
+        if (field === 'CS') {
           csCount += 1;
           csList.push(student[0]);
-        } else if (s === 'SWE') {
+        } else if (field === 'SWE') {
           sweCount += 1;
           sweList.push(student[0]);
         }
       }
     }
-    console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
-    console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
-    const allCount = items.length;
-    return {
-      allCount,
-      csCount,
-      sweCount,
-      csList,
-      sweList,
-    };
+    const reportParts = [
+      'This is the list of our students',
+      `Number of students: ${students.length}`,
+      `Number of students in CS: ${csCount}. List: ${csList.join(', ')}`,
+      `Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`,
+    ];
+    console.log(reportParts.join('\n'));
   } catch (err) {
     throw new Error('Cannot load the database');
   }
