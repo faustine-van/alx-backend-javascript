5cbf4b656c3c2a401f7ccd7a81c4f4113bc445bc
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-28 22:52:39 +0200

    updated

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,4 +7,13 @@ describe('calculateNumber', () => {
   it('should return 4', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
 });
