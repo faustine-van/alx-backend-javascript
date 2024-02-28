434f75ad8b5384fc5cd8278487ae2e01c1ce5534
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-28 22:47:33 +0200

    updated

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -19,19 +19,4 @@ describe('calculateNumber', () => {
   it('should return 6', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it('should return NaN when no args present', () => {
-    assert.strictEqual(calculateNumber(), NaN);
-  });
-  it('should return NaN when no args present', () => {
-    assert.strictEqual(calculateNumber(1), NaN);
-  });
-  it('should return NaN when no arg present', () => {
-    assert.strictEqual(calculateNumber(2), NaN);
-  });
-  it('should return NaN when arg present but string', () => {
-    assert.strictEqual(calculateNumber('string', 8), NaN);
-  });
-  it('should return NaN when args present but list', () => {
-    assert.strictEqual(calculateNumber(5, [1, 2, 3]), NaN);
-  });
 });
