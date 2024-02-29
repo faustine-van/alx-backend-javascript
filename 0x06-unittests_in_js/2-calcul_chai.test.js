2cb3bbf8abd7bb3c12df021c878da9507af4dc39
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-29 09:57:51 +0200

    add more tests

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -18,19 +18,19 @@ describe('calculateNumber', () => {
   });
   describe('subtract', () => {
     it('should return 4', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(4);
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
     it('should return 10', () => {
-      expect(calculateNumber('SUBTRACT', 4, 14)).to.equal(10);
+      expect(calculateNumber('SUBTRACT', 4, 14)).to.equal(-10);
     });
-    it('should return -1', () => {
-      expect(calculateNumber('SUBTRACT', 1.4, 0)).to.equal(-1);
+    it('should return 1', () => {
+      expect(calculateNumber('SUBTRACT', 1.4, 0)).to.equal(1);
     });
-    it('should return -1 when float', () => {
-      expect(calculateNumber('SUBTRACT', 2.99999, 2.09)).to.equal(-1);
+    it('should return 1 when float', () => {
+      expect(calculateNumber('SUBTRACT', 2.99999, 2.09)).to.equal(1);
     });
-    it('should return -1 when float', () => {
-      expect(calculateNumber('SUBTRACT', 5.12, 2.09)).to.equal(-3);
+    it('should return 3 when float', () => {
+      expect(calculateNumber('SUBTRACT', 5.12, 2.09)).to.equal(3);
     });
   });
   describe('divide', () => {
