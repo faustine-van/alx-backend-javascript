e14181200524353c81ec336cf3671141ab11a50f
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-29 15:15:59 +0200

    hook and spy

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+// module 5-payment.test.js
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToAPI = require('./5-payment');
+
+describe('sendPaymentRequestToAPI', () => {
+  // Setup before each test case
+  let spyConsole;
+  beforeEach(() => {
+    spyConsole = sinon.spy(console, 'log');
+  });
+  // Setup after each test case
+  // eslint-disable-next-line jest/no-hooks
+  afterEach(() => {
+    spyConsole.restore();
+  });
+  it('test will call sendPaymentRequestToAPI with 100, and 20', () => {
+    sendPaymentRequestToAPI(100, 20);
+    sinon.assert.calledOnceWithExactly(spyConsole, 'The total is: 120');
+    // sinon.assert.spyConsole.calledOnce();
+  });
+  it('test will call sendPaymentRequestToAPI with 10, and 10', () => {
+    sendPaymentRequestToAPI(10, 10);
+    sinon.assert.calledOnceWithExactly(spyConsole, 'The total is: 20');
+  });
+});
