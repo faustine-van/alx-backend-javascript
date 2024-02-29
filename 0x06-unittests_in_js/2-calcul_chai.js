c52b2636c88693dfc71f55ba789b2c130eafc053
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-29 10:02:19 +0200

    add more tests

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -4,7 +4,7 @@ function calculateNumber(type, a, b) {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
   } if (type === 'SUBTRACT') {
-    return Math.round(b) - Math.round(a);
+    return Math.round(a) - Math.round(b);
   } if (type === 'DIVIDE') {
     if (Math.round(b) === 0) {
       return 'Error';
@@ -12,4 +12,5 @@ function calculateNumber(type, a, b) {
     return Math.round(a) / Math.round(b);
   }
 }
+// console.log(calculateNumber('DIVIDE', -14.9999, 2));
 module.exports = calculateNumber;
