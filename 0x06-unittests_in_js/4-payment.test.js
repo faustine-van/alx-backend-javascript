39ed883c8544b57166704a9a5ee815d963ea9d0a
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-29 14:34:34 +0200

    test using spies

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,14 +3,23 @@ the same as Utils.calculateNumber('SUM', 100, 20) */
 const sinon = require('sinon');
 // const assert = require('assert');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
-const spy = sinon.spy(sendPaymentRequestToApi);
-spy(100, 20);
+// Stub Utils.calculateNumber to always return 10
+const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
 
-const stub = sinon.stub(Utils, 'calculateNumber');
-stub.alwaysReturned(10);
-stub.calledWith('SUM', 100, 20);
+// Create a spy for console.log
+const consoleSpy = sinon.spy(console, 'log');
+
+// call function
+sendPaymentRequestToApi(100, 20);
+
+// Verify that Utils.calculateNumber was called with the correct arguments
+sinon.assert.calledWithExactly(stub, 'SUM', 100, 20);
+
+// Verify that console.log was called with the correct message
+sinon.assert.calledWithExactly(consoleSpy, 'The total is: 10');
 
 // assert.strictEqual(spy.returnValues[0], stub.returned());
 stub.restore();
+consoleSpy.restore();
