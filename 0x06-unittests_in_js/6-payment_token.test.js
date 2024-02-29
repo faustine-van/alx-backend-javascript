d25d14e4a9d1991de1e524c1dc40e7c825f10f06
faustine-van <faustinemuhayemariya44@gmail.com>
2024-02-29 16:34:38 +0200

    test asynchrous

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,15 +2,20 @@ const assert = require('assert');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the object', () => new Promise((done) => {
-    getPaymentTokenFromAPI(true)
-      .then((data) => {
-        const expected = { data: 'Successful response from the API' };
-        assert.deepStrictEqual(data, expected);
-        done(); // Indicate test completion to Mocha
-      })
-      .catch((error) => {
-        done(error); // Pass error to Mocha
-      });
+  it('should return a resolved promise with the expected object', () => new Promise((done) => {
+    // Call the function and wait for the promise to resolve
+    getPaymentTokenFromAPI(true).then((result) => {
+      // Define the expected object
+      const expected = { data: 'Successful response from the API' };
+
+      // Compare the result with the expected object
+      assert.deepStrictEqual(result, expected);
+
+      // Call done to indicate that the test is complete
+      done();
+    }).catch((error) => {
+      // If there is an error, fail the test
+      done(error);
+    });
   }));
 });
