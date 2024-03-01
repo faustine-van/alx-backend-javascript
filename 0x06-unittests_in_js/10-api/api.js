d350effdd39e885817476b0079eba83237174a78
faustine-van <faustinemuhayemariya44@gmail.com>
2024-03-01 03:23:33 +0200

    add more test

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,8 +1,8 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
