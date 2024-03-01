e3e80784f125a737f475dbcba06ad9e5587f86fb
faustine-van <faustinemuhayemariya44@gmail.com>
2024-03-01 02:14:52 +0200

    updated and add more test

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
-app.get('/cart/:id(\\d+)', (req, res) => {
+app.get('/cart/:id', (req, res) => {
   const ids = parseInt(req.params.id, 10);
   // eslint-disable-next-line no-restricted-globals
   if (!isNaN(ids)) {
