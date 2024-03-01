fbcc6b85b83e6e34b9dabc5f827abf776736ae42
faustine-van <faustinemuhayemariya44@gmail.com>
2024-03-01 02:10:34 +0200

    updated and add more test

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 app.get('/cart/:id(\\d+)', (req, res) => {
   const ids = parseInt(req.params.id, 10);
-  if (!ids.isNaN) {
+  if (!isNaN(ids)) {
     res.send(`Payment methods for cart ${ids}`);
   }
 });
