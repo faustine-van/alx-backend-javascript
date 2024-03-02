b8939655a65dbc4993556cf74ac1bb36d981d772
faustine-van <faustinemuhayemariya44@gmail.com>
2024-03-02 15:05:10 +0200

    remove space

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -14,8 +14,9 @@ class StudentsController {
         report.push(`Number of students in SWE: ${data.SWE.length}. List:${data.SWE}`);
 
         response.send(report.join('\n'));
-      }).catch((error) => {
-        console.error('Error:', error); // Log the error
+      })
+      // eslint-disable-next-line no-unused-vars
+      .catch((error) => {
         response.status(500).send('Cannot load the database');
       });
   }
@@ -25,15 +26,15 @@ class StudentsController {
     readDatabase(file)
       .then((data) => {
         if (field === 'CS') {
-          response.status(200).send(`List: ${data.CS}`);
+          response.status(200).send(`List:${data.CS}`);
         } else if (field === 'SWE') {
           response.status(200).send(`List:${data.SWE}`);
         } else {
           response.status(500).send('Major parameter must be CS or SWE');
         }
       })
+      // eslint-disable-next-line no-unused-vars
       .catch((error) => {
-        console.error('Error:', error); // Log the error
         response.status(500).send('Cannot load the database');
       });
   }
